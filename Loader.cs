bec8ccbb2cc2b54fe233cf6452f118b2521c27c2
zhangxingyu <rpm@live.com>
2021-04-27 01:53:31 +0800

    increase health;

---
+++ Loader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace HeXas_Darkwood_Hack
+{
+    public class Loader
+    {
+        public static void Init()
+        {
+            _gameObject = new GameObject();
+            _gameObject.AddComponent<Main>();
+            GameObject.DontDestroyOnLoad(_gameObject);
+        }
+        public static void Unload()
+        {
+            _Unload();
+        }
+        private static void _Unload()
+        {
+            GameObject.Destroy(_gameObject);
+        }
+        private static GameObject _gameObject;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace HeXas_Darkwood_Hack
+{
+    class Main : MonoBehaviour
+    {
+        public void Start()
+        {
+            _player = FindObjectOfType<Player>();
+        }
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.U))
+            {
+                _player.upgradeHealth();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Delete)) // Will just unload our DLL
+            {
+                Loader.Unload();
+            }
+
+        }
+        public void OnGUI()
+        {
+            //GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 150f, 50f), "GAME INJECTED"); // Should work and when injected you will see this text in the middle of the screen
+        }
+        private Player _player;
+    }
+}
